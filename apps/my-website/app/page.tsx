14d4994c5e145c84819fa5e85833954d18667c40
Atharva Chavan <vercentlabs@gmail.com>
2026-07-17 09:13:41 +0530

    Remove unused my-website app

--- apps/my-website/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
- 
-export default function Page() {
-  return <>
-  <Image src="/file.svg" alt="Profile" width={100} height={100} />
-  <h1>Hello, Next.js!</h1>
-  </>
-}
-
